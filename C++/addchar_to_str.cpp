676acca8960b640be1fcd4db48e12f094addc85b
lokanathmeher19 <meherlokanath314@gmail.com>
2026-02-21 02:11:24 +0530

    vs_code

---
+++ C++/addchar_to_str.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    string s;
+
+    cout << "Enter a character: ";
+    cin >> c;
+
+    cout << "Enter a word: ";
+    cin >> s;
+
+    cout << "Result: " << c << s << c << endl;
+
+    return 0;
+}
---
+++ C++/sumof2num.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    
+    cout<<a+b<<endl;
+
+    return 0;
+}
