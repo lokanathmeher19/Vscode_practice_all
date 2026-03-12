fab001d172481137f162d747fbb64ace46977a82
lokanathmeher19 <meherlokanath314@gmail.com>
2026-03-13 00:13:13 +0530

    Python

---
+++ Python/Nested-condition/special-11.py
@@ -0,0 +1,7 @@
+n=int(input())
+
+if n%11==0 or n%11==1:
+    print("Special Eleven")
+else:
+    print("Normal Number")
+    
---
+++ Python/basic_py/Discount.py
@@ -0,0 +1,7 @@
+a=input()
+b=input()
+
+if a[:3]=="DIS" or b[:3]=="DIS":
+    print("Discount")
+else:
+    print("No Discount")
