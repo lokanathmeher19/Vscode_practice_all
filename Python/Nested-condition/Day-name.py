56b55d99f5fcf79c982bdfa206c85e4c69ef4e97
lokanathmeher19 <meherlokanath314@gmail.com>
2026-03-14 00:30:08 +0530

    Vs-code

---
+++ Python/Nested-condition/Day-name.py
@@ -0,0 +1,16 @@
+daynum=int(input("Enter the day number (1-7): "))
+
+if daynum==1:
+    print("Monday")
+elif daynum==2:
+    print("Tuesday")
+elif daynum==3:
+    print("Wednesday")
+elif daynum==4:
+    print("Thursday")
+elif daynum==5:
+    print("Friday")
+elif daynum==6:
+    print("Saturday")
+else:
+    print("Sunday")
