ba43c978bed8a031f94a127f243df5c949d5425e
lokanathmeher19 <meherlokanath314@gmail.com>
2026-04-07 23:01:19 +0530

    Task-2

---
+++ Java/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+     public static void main(String[] args) {
+        int age = 20;
+        String name = "Biswajit";
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
