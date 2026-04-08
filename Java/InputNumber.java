3ac2810175162e8b11fbf226bc31971f00499916
lokanathmeher19 <meherlokanath314@gmail.com>
2026-04-08 22:52:27 +0530

    Java

---
+++ Java/InputNumber.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class InputNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        System.out.println("You entered: " + num);
+    }
+}
