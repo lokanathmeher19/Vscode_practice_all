e61fad7042f29c9561a621d7761fd9fcdc537b20
Lokanath Meher <meherlokanath314@gmail.com>
2026-04-25 22:59:33 +0530

    JAVA

---
+++ Java/03_Loops/PalindromeNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int original = num;
+        int rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (original == rev) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+    }
+}
